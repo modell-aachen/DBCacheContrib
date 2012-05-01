adddf2be8e814c84d3df2c72b344f424ebc11036
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/DBCacheContrib@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/DBCacheContrib/build.pl
+++ lib/Foswiki/Contrib/DBCacheContrib/build.pl
@@ -2,22 +2,22 @@
 
 # Standard preamble
 BEGIN {
-  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
-    unshift @INC, $pc;
-  }
+    foreach my $pc ( split( /:/, $ENV{FOSWIKI_LIBS} ) ) {
+        unshift @INC, $pc;
+    }
 }
 
 use Foswiki::Contrib::Build;
 
 package DBCacheBuild;
 
-@DBCacheBuild::ISA = ( "Foswiki::Contrib::Build" );
+@DBCacheBuild::ISA = ("Foswiki::Contrib::Build");
 
 sub new {
-  my $class = shift;
-  return bless( $class->SUPER::new( "DBCacheContrib" ), $class );
+    my $class = shift;
+    return bless( $class->SUPER::new("DBCacheContrib"), $class );
 }
 
 $build = new DBCacheBuild();
 
-$build->build($build->{target});
+$build->build( $build->{target} );
